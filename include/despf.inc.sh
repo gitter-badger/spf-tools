ddad971712eef178ae0d50e9ee4eafa7f439b892
Jan Sarenik <jasan@jasan.tk>
2015-12-12 19:18:23 +0100

    Rename timeout to fix ash tests

--- include/despf.inc.sh
+++ include/despf.inc.sh
@@ -2,7 +2,7 @@ DNS_TIMEOUT=${DNS_TIMEOUT:-"2"}
 
 mdrill() {
   TYPE=${1}
-  timeout $DNS_TIMEOUT drill "$@" | sed -n "/^[^;]/{/$TYPE/p}" \
+  mytimeo $DNS_TIMEOUT drill "$@" | sed -n "/^[^;]/{/$TYPE/p}" \
       || { echo "DNS lookup error!" >&2; cleanup; false; }
 }
 
